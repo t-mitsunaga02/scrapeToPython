26931e0e7f7bf07a1926e4a6b89eb6231aa4fb2b
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-04-20 20:43:00 +0900

    first commit

---
+++ HttpTrigger1/test.py
@@ -0,0 +1,18 @@
+# PYTHON_AGE_01-1
+
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+# PYTHON_AGE_01-2
+
+# 生年月日
+d0 = date(1998, 5, 12)
+
+# 現在の日付
+d1 = date.today()
+
+# 経過時間
+dy = relativedelta(d1, d0)
+
+print(dy)
+
