515101837ba9c9c5c70a03632cc30ca70d6a8e58
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-04-24 13:42:21 +0900

    modify commit

--- HttpTrigger1/test.py
+++ HttpTrigger1/test.py
@@ -5,14 +5,15 @@ from dateutil.relativedelta import relativedelta
 
 # PYTHON_AGE_01-2
 
-# 生年月日
-d0 = date(1998, 5, 12)
+def calc():
+    # 生年月日
+    d0 = date(1998, 5, 12)
 
-# 現在の日付
-d1 = date.today()
+    # 現在の日付
+    d1 = date.today()
 
-# 経過時間
-dy = relativedelta(d1, d0)
+    # 経過時間
+    dy = relativedelta(d1, d0)
 
-print(dy)
+    print(dy)
 
