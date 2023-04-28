1ec36f9b2a1f1351592c92c0db8ad3b23dea6076
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-04-28 17:45:03 +0900

    mody commit

--- HttpTrigger1/test.py
+++ HttpTrigger1/test.py
@@ -2,9 +2,11 @@ import logging
 
 import azure.functions as func
 
-def main(req: func.HttpRequest,outputblob:func.Out[str]) -> func.HttpResponse:
+def main(req: func.HttpRequest,outputblob:func.Out[str],inputblob:func.InputStream[str],copyblob:func.Out[str]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    # ファイルのバックアップ
+    copyblob.set(inputblob)
     # 出力バインドしたファイルに変数の値を書き込みます
     outputtext = outputblob.set("テスト")
 
